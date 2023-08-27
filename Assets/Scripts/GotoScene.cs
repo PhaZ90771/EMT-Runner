1c6b499e0f2c03a9a936dd24a41bbc682daebdd6
Benjamin Shutt <shuttben@gmail.com>
2023-08-26 23:40:37 -0700

    Old ScoreManager now gets deleted when loading into the Gameplay scene.

--- Assets/Scripts/GotoScene.cs
+++ Assets/Scripts/GotoScene.cs
@@ -7,6 +7,15 @@ public class GotoScene : MonoBehaviour
 
     public void Goto()
     {
+        if (SceneIndex == 1) // If goto gameplay scene
+        {
+            var scoreManager = FindAnyObjectByType<ScoreManager>();
+            if (scoreManager)
+            {
+                Destroy(scoreManager.gameObject); // Delete ScoreManager if it already exists
+            }
+        }
+
         SceneManager.LoadScene((int)SceneIndex);
     }
 }
